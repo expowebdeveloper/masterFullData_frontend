29af1518bd30bd215f8a0266f8b78e3ddaa90fa0
webexpowebdeveloper <expowebdeveloper@gmail.com>
2024-01-29 16:53:44 +0530

    implemented Delete and edit APi

--- src/components/dimensions/AddProperty.jsx
+++ src/components/dimensions/AddProperty.jsx
@@ -1,57 +1,114 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MdButton from "../common/atomic/MdButton";
 import { Col, Row, Form, Button } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
+import { assignProperty } from "../../store/slices/dimensionsSlice";
+import EditDeleteProperty from "./EditDeleteProperty";
 
-const AddProperty = ({ handlepropShow, handleShow }) => {
-  const { nodeProperties } = useSelector((state) => state.dimensionData);
-  console.log(nodeProperties, "nodeProperties");
+const AddProperty = ({
+  handlepropShow,
+  handleShow,
+  currentDimension,
+  selectedNode,
+  selectedPropertyField,
+  allNodeProperties,
+  setIsAssignProperty,
+  isPropertyAdded,
+  listProperties
+}) => {
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty, isValid, isSubmitting },
+  } = useForm({});
+
+  const onSubmit = (data) => {
+    console.log(data, "jjjj");
+    let propertiesValue = {};
+    for (let key in data) {
+      console.log(key, "ll");
+      if (data[key]) {
+        propertiesValue = {
+          ...propertiesValue,
+          [key]: data[key],
+        };
+      }
+    }
+    let payloadData = {
+      dimension: currentDimension,
+      assignments: [
+        {
+          node_name: selectedNode,
+          properties: propertiesValue,
+        },
+      ],
+    };
+
+    dispatch(assignProperty(payloadData));
+  };
+
   return (
     <div className="col-md-9">
       <div className="heading p-3">
-        <h2 className="text-center m-0">Test 01 Dimension</h2>
+        <h2 className="text-center m-0">{currentDimension}</h2>
       </div>
       <div className="uploadFileCSV">
         <Form.Control type="file" placeholder="Enter First Name" />
         <b>Upload CSV or JSON File</b>
       </div>
-      <div className="propertyListing p-4 mt-4">
+    { isPropertyAdded? <div className="propertyListing p-4 mt-4">
         <Button
           variant="primary"
-          onClick={handlepropShow}
+          onClick={() => setIsAssignProperty(true)}
           className="PropertyBtn"
         >
           Assign Property
         </Button>
-        {/* <Button variant="primary" onClick={handleShow} className="ms-2">
-        Delete Popup
-      </Button> */}
-        {nodeProperties.length>0?<Form className="mt-4">
-          {nodeProperties?.map((item, index) => {
-            return (
-              <React.Fragment key={index}>
-                <Row>
-                  <Col md>
-                    <Form.Group className="mb-3" controlId="formBasicFName">
-                      <Form.Label>{item.name}</Form.Label>
-                      <input
-                        type={item.type}
-                        className="common-field"
-                        name={item.name}
-                        placeholder=""
-                      />
-                    </Form.Group>
-                  </Col>
-                </Row>
-              </React.Fragment>
-            );
-          })}
+        {allNodeProperties.length > 0 ? (
+          <form className="mt-4" onSubmit={handleSubmit(onSubmit)} noValidate>
+            {allNodeProperties?.map((item, index) => {
+              return (
+                <React.Fragment key={index}>
+                  <Row>
+                    <Col md>
+                      <Form.Group className="mb-3" controlId="formBasicFName">
+                        <Form.Label>{item.name}</Form.Label>
+                        <input
+                          type={item?.type}
+                          className="common-field"
+                          name={item.name}
+                          value={item?.defaultValue}
+                          placeholder=""
+                          {...register(item?.name, {
+                            required: {
+                              value: true,
+                              message: `${item?.name} is required`,
+                            },
+                          })}
+                        />
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                </React.Fragment>
+              );
+            })}
 
-          <Button variant="primary" type="submit">
-          Submit
-        </Button>
-        </Form>:""}
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </form>
+        ) : (
+          ""
+        )}
       </div>
+      :
+      <EditDeleteProperty
+      listProperties={listProperties}
+      currentDimension={currentDimension}
+      />
+      }
     </div>
   );
 };
---
+++ src/components/dimensions/EditDeleteProperty.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { MdEdit, MdDelete } from "react-icons/md";
+import DeletePropertyModal from "../singleDimensions/DeletePropertyModal";
+import { useDispatch } from "react-redux";
+import { deleteProperty } from "../../store/slices/dimensionsSlice";
+
+const EditDeleteProperty = ({ listProperties,currentDimension}) => {
+  const [show, setShow] = useState(false);
+  const [selectedProperty,setSelectedProperty]=useState('')
+  const dispatch =useDispatch()
+  const deleteModal = (item) => {
+    setShow(true);
+    setSelectedProperty(item)
+  };
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  const confirmDelete=()=>{
+    let data={
+        property_name:selectedProperty,
+        dimension:currentDimension
+    }
+    dispatch(deleteProperty(data))
+    setShow(false)
+  }
+  return (
+    <>
+      <Container>
+        <Row>
+          {listProperties.map((item) => {
+            return (
+              <>
+                <Col>
+                  <div className="property-edit-delete">
+                    {item.label}
+                    <div>
+                      <span>
+                        {" "}
+                        <MdEdit />
+                      </span>
+                      <span onClick={() => deleteModal(item.label)}>
+                        {" "}
+                        <MdDelete />
+                      </span>
+                    </div>
+                  </div>
+                </Col>
+              </>
+            );
+          })}
+        </Row>
+      </Container>
+      <DeletePropertyModal
+        show={show}
+        handleClose={handleClose}
+        heading={"Property"}
+        message={"Are you sure, you want to delete the Property?"}
+        confirmDelete={confirmDelete}
+      />
+    </>
+  );
+};
+
+export default EditDeleteProperty;
